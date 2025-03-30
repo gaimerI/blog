e2134682df22ce936fa717512afcbbcc05370411
gaimerI17 <gaimeri255@gmail.com>
2025-03-30 17:12:48 +0300

    Create const.js

---
+++ forum/const.js
@@ -0,0 +1,6 @@
+export const backendURL = "https://gaimeri17-forumtopicmanagemement.web.val.run";
+export const registerAuthURL = "https://gaimeri17-forumusermanagement.web.val.run/register";
+export const loginAuthURL = "https://gaimeri17-forumusermanagement.web.val.run/login";
+export const userDataAuthURL = "https://gaimeri17-forumusermanagement.web.val.run/users";
+export const commentBackendURL = "https://gaimeri17-forumcommentmanagement.web.val.run";
+export const reactionBackendURL = "https://gaimeri17-forumtopicmanagemement.web.val.run/react";
